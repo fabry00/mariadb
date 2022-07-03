09198c5d2ed9f32128c70f190a63288479382a24
Fabrizio Faustinoni <fabry_00@hotmail.com>
2022-07-03 19:31:28 +0200

    init

--- cluster01/startNode01.sh
+++ cluster01/startNode01.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-
 HOST="127.0.0.1"
 NODE_NAME="cluster01-node-01"
+SERVER_ID_PER_CLUSTER=1
+CONF=$(pwd)/conf
+mkdir -p ${CONF}
 MYQSL_PORT=7500
 GALERA_PORT=7501
 IST_PORT=7502
 SST_PORT=7503
+echo "Stopping ${NODE_NAME}"
+docker stop -t 30 ${NODE_NAME} || true
 docker rm -f ${NODE_NAME} || true
+echo "Starting ${NODE_NAME}"
 docker run -d --name ${NODE_NAME} \
   --network host \
+  -v ${CONF}/my_custom.cnf:/opt/bitnami/mariadb/conf/my_custom.cnf:ro \
   -e MARIADB_GALERA_CLUSTER_NAME=my_galera \
   -e MARIADB_GALERA_MARIABACKUP_USER=my_mariabackup_user \
   -e MARIADB_GALERA_MARIABACKUP_PASSWORD=my_mariabackup_password \
@@ -20,7 +26,7 @@ docker run -d --name ${NODE_NAME} \
   -e MARIADB_REPLICATION_USER=my_replication_user \
   -e MARIADB_REPLICATION_PASSWORD=my_replication_password \
   -e MARIADB_PORT_NUMBER=${MYQSL_PORT} \
-  -e MARIADB_EXTRA_FLAGS="--wsrep-node-incoming-address=${MYQSL_PORT};--wsrep_node_name=${NODE_NAME};--wsrep_provider_options=ist.recv_addr=${HOST}:${IST_PORT};ist.recv_bind=0.0.0.0:${IST_PORT} --wsrep_node_incoming_address=${HOST}  --wsrep_sst_receive_address=${HOST}:${SST_PORT} --wsrep_node_address=${HOST}:${GALERA_PORT}" \
+  -e MARIADB_EXTRA_FLAGS="--server-id=${SERVER_ID_PER_CLUSTER};--log-slave-updates=ON;--wsrep-node-incoming-address=${MYQSL_PORT};--wsrep-node-name=${NODE_NAME};--wsrep_provider_options=ist.recv_addr=${HOST}:${IST_PORT};ist.recv_bind=0.0.0.0:${IST_PORT} --wsrep_node_incoming_address=${HOST}  --wsrep_sst_receive_address=${HOST}:${SST_PORT} --wsrep_node_address=${HOST}:${GALERA_PORT}" \
   -p ${GALERA_PORT}:${GALERA_PORT} \
   -p ${IST_PORT}:${IST_PORT} \
   -p ${SST_PORT}:${SST_PORT} \
--- cluster02/startNode02.sh
+++ cluster02/startNode02.sh
@@ -22,7 +22,7 @@ docker run -d --name ${NODE_NAME} \
   -e MARIADB_REPLICATION_USER=my_replication_user \
   -e MARIADB_REPLICATION_PASSWORD=my_replication_password \
   -e MARIADB_PORT_NUMBER=${MYQSL_PORT} \
-  -e MARIADB_EXTRA_FLAGS="--wsrep-node-incoming-address=${MYQSL_PORT};--wsrep_node_name=${NODE_NAME};--wsrep_provider_options=ist.recv_addr=${HOST}:${IST_PORT};ist.recv_bind=0.0.0.0:${IST_PORT} --wsrep_node_incoming_address=${HOST}  --wsrep_sst_receive_address=${HOST}:${SST_PORT} --wsrep_node_address=${HOST}:${GALERA_PORT}" \
+  -e MARIADB_EXTRA_FLAGS="--wsrep-node-incoming-address=${MYQSL_PORT};--wsrep-node-name=${NODE_NAME};--wsrep_provider_options=ist.recv_addr=${HOST}:${IST_PORT};ist.recv_bind=0.0.0.0:${IST_PORT} --wsrep_node_incoming_address=${HOST}  --wsrep_sst_receive_address=${HOST}:${SST_PORT} --wsrep_node_address=${HOST}:${GALERA_PORT}" \
   -p ${GALERA_PORT}:${GALERA_PORT} \
   -p ${IST_PORT}:${IST_PORT} \
   -p ${SST_PORT}:${SST_PORT} \
